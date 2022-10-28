d59263a507ba60b0988dd74a08f626c261b85c1e
adyluna <adylunaleite@gmail.com>
2022-10-28 10:24:02 -0300

    Req 02 ok

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    display_name: DataTypes.STRING,
+    displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
