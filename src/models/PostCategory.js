643eb2029ad5a283464c72ebb096a8ad57f98127
adyluna <adylunaleite@gmail.com>
2022-10-31 02:14:37 -0300

    Req 11 ok

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,27 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
-  const PostsCategorie = sequelize.define('PostsCategorie',
-  {},
+  const PostCategory = sequelize.define('PostCategory',
+  {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  },
   {
     timestamps: false,
     tableName: 'posts_categories',
     underscored: true,
   });
 
-  PostsCategorie.associate = (models) => {
+  PostCategory.associate = (models) => {
     models.Category.belongsToMany(models.BlogPost, {
       as: 'blog_posts',
-      through: PostsCategorie,
+      through: PostCategory,
       foreignKey: 'postId',
       otherKey: 'categorieId'
     });
 
     models.BlogPost.belongsToMany(models.Category, {
       as: 'categories',
-      through: PostsCategorie,
+      through: PostCategory,
       foreignKey: 'categorieId',
       otherKey: 'postId'
     });
   }
 
-  return PostsCategorie;
+  return PostCategory;
 };
